616318e7ffb0903a1b92d7cf454a3d718279e981
StefaniaPozzi <stefania.pozzi.it@gmail.com>
2023-05-17 15:50:06 +0200

    retieved value from smartcontract

--- components/Entrance.jsx
+++ components/Entrance.jsx
@@ -1,9 +1,24 @@
 "use client";
-import { useAddress } from "@thirdweb-dev/react";
-
+import {
+  useAddress,
+  useContract,
+  useChainId,
+  useContractRead,
+} from "@thirdweb-dev/react";
+import { contractAddress, contractABI } from "../constants/index.js";
 export default function () {
+  let dataFromContract = "";
   const address = useAddress();
+  const chainId = useChainId();
+  if (chainId) {
+    const currentContractAddress = contractAddress[chainId][0];
+    const { contract } = useContract(currentContractAddress, contractABI);
+
+    const { data: costant } = useContractRead(contract, "getConstant");
+    dataFromContract = costant;
+    console.log(costant);
+  }
   if (!address)
-    return <div>Connect to a wallet to enjoy my amazing features</div>;
-  return "entrance";
+    return <div>Connect to a wallet to enjoy my amazing features </div>;
+  return <p>Getting data from the smartcontract</p>;
 }
---
+++ constants/index.js
@@ -0,0 +1,7 @@
+const contractAddress = require("./contractAddress.json");
+const contractABI = require("./contractABI.json");
+
+module.exports = {
+  contractABI,
+  contractAddress,
+};
