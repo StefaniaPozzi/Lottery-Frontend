25ec8fdcc6b70fab5814572253ca5bbd6f35a99c
StefaniaPozzi <stefania.pozzi.it@gmail.com>
2023-05-19 11:19:51 +0200

    added ticket price

--- components/Entrance.jsx
+++ components/Entrance.jsx
@@ -1,24 +1,39 @@
-"use client";
 import {
   useAddress,
   useContract,
   useChainId,
   useContractRead,
 } from "@thirdweb-dev/react";
+import { useEffect, useState, useRef } from "react";
 import { contractAddress, contractABI } from "../constants/index.js";
-export default function () {
-  let dataFromContract = "";
+import { ethers } from "ethers";
+
+export default function MyComponent() {
   const address = useAddress();
   const chainId = useChainId();
-  if (chainId) {
-    const currentContractAddress = contractAddress[chainId][0];
-    const { contract } = useContract(currentContractAddress, contractABI);
+  const [ticketPrice, setTicketPrice] = useState(0);
 
-    const { data: costant } = useContractRead(contract, "getConstant");
-    dataFromContract = costant;
-    console.log(costant);
-  }
+  const currentContractAddress = chainId ? contractAddress[chainId][0] : null;
+  const { contract } = useContract(currentContractAddress, contractABI);
+  //rebuild to see changes
+  const { data } = useContractRead(contract, "getTicketPrice");
+  const resultTicketPrice = data ? data.toString() : null;
+
+  useEffect(() => {
+    if (chainId && address) {
+      setTicketPrice(resultTicketPrice);
+    }
+  }, [address]);
+
   if (!address)
-    return <div>Connect to a wallet to enjoy my amazing features </div>;
-  return <p>Getting data from the smartcontract</p>;
+    return <div>Connect to a wallet to enjoy my amazing features</div>;
+
+  return (
+    <div>
+      Current ticket price:
+      <p>
+        {ticketPrice ? ethers.utils.formatUnits(ticketPrice, "ether") : ""} ETH
+      </p>
+    </div>
+  );
 }
