af674d6dfe8429e75ec1a59f63913265b208df48
StefaniaPozzi <stefania.pozzi.it@gmail.com>
2023-06-02 15:23:44 +0200

    added mui

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,17 +1,26 @@
 "use client";
-import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
+import * as React from "react";
+import { ConnectWallet } from "@thirdweb-dev/react";
+
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
 
 export default function Header() {
   return (
-    <div className="border-b-2  border-amber-600">
-      <div className="grid grid-cols-4 gap-4">
-        <div className="text-2xl"> Decentralized Lottery</div>
-        <div></div>
-        <div></div>
-        <div>
+    <div>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <Typography variant="h2" gutterBottom>
+            Habit maker
+          </Typography>
+          <Typography variant="h6" gutterBottom>
+            Just make habits
+          </Typography>
+        </Grid>
+        <Grid item xs={4}>
           <ConnectWallet />
-        </div>
-      </div>
+        </Grid>
+      </Grid>
     </div>
   );
 }
